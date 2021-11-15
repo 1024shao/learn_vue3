cd4ada7ad79420c2128949dbe8ad59ebbaa66fcc
1024shao <3059680677@qq.com>
2021-11-15 17:03:27 +0800

    reload redeme

--- vue3_webpack/src/App.vue
+++ vue3_webpack/src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-    <h2>{{name}}</h2>
-    <h2>{{age}}</h2>
-  </div>
+  <Demo />
 </template>
 
 <script>
-import { ref } from 'vue'
-// 为什么 vue3需要进行引入的方式实现响应式
-// 1. 减少体积,不需要花费资源对原本不需要进行响应式的数据进行更改
-// 2.
+import Demo from './components/Demo.vue'
 export default {
   name: 'App',
-  
+  components: {
+    Demo
+  },
+
 }
 </script>
---
+++ vue3_webpack/src/components/Demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{sum}}</h2>
+    <button @click="sum++">点我+1</button>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue'
+export default {
+  setup() {
+    let sum = ref(0)
+    watch(sum, () => {
+      console.log('sum')
+    }, {
+      immediate: true,
+      deep: true,
+    })
+    return {
+      sum
+    }
+
+  }
+
+}
+</script>
+
+<style>
+</style>
